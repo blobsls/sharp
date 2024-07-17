ade2eb24c6d18692c1aca86c4195b707b43b0b54
pyp <166371237+pyp-m@users.noreply.github.com>
2024-07-17 10:39:59 -0400

    Add files via upload

---
+++ asyncio/AsyncDiscord.py
@@ -0,0 +1,10 @@
+import os
+import code
+
+# code compiles.
+code.interact(local=os.devnull)
+code.compile_command == ()
+# tokens
+def token(self, tokenize):
+    tokenize(type)
+    code == {str,function}
---
+++ libs/rst-26x-34x-lim__py34.py
@@ -0,0 +1,55 @@
+#kmx imports
+import py_compile
+import libs
+
+# compile lib
+def token(self):
+    token.__class__ = {}
+    pass
+# compile lib system (main)
+py_compile.main((libs) == (token) or (py_compile._get_default_invalidation_mode))
+if token == False:
+    py_compile is libs - token is token
+if libs == py_compile:
+    token == 'tk'
+else:
+    if libs == py_compile:
+        py_compile == token # dir
+if token == max:
+    str == compile
+else:
+    if token == 0:
+        Warning (print('token null value reached.'))
+if token.__class__ == filter.mro:
+    print('lib class is mro object, cant compile')
+elif token.__class__ == libs:
+    token == libs('typelib')
+elif token.__eq__ == False:
+    token is filter.mro or Warning (print('False argument'))
+elif token == py_compile.compile:
+    token is True and compile
+elif token == libs (max):
+    token is Warning
+elif libs == token:
+    token is False
+elif py_compile.__doc__ == token.__dir__:
+    py_compile and token('yielding process') - Warning
+# lib processing
+def lib(dict__m__):
+    py_compile == True
+    if lib == False:
+        dict__m__ is False
+    if lib == True:
+        dict__m__ is True
+pass
+Process = property in lib
+- Process in token == False
+- Process in lib == True
+Process ([1]) == ([token.__str__] or [lib.__doc__])
+# make sure the token is not multiplied
+token ((lib == __dict__),(Process in lib == lib.__type_params__ and lib.__code__))
+if token == (2 in 4):
+    token is Warning (print('token multiplied, process stopped'))
+def asset(self):
+    # i couldnt code this PLZ send me help
+    pass
